0db555175888f5d536bedf7e38fc7ea9e61dc445
ImanolLow <imanolmolinuevo@gmail.com>
2026-03-17 08:09:41 +0100

    Update: GameManager, MainScenes, UI, and Planet assets

--- orbitalyte/MainScenes/main_menu.gd
+++ orbitalyte/MainScenes/main_menu.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
-
 func _on_play_pressed() -> void:
+	FadeTransitions.transition()
+	await FadeTransitions.on_transition_finished
 	get_tree().change_scene_to_file("res://MainScenes/Level1.tscn")
-
 
 func _on_options_pressed() -> void:
 	pass
 
 
 func _on_quit_pressed() -> void:
+	FadeTransitions.transition()
+	await FadeTransitions.on_transition_finished
 	get_tree().quit()
